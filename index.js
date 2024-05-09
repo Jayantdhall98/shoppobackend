2e1b530a68b2035e2ab0c2f4e87c3c269ad64f2c
jayantdhall98 <jayantdhall98@gmail.com>
2024-05-10 01:39:34 +0530

    nodeindexxjs4rthddd

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ const sessionStorage= MongoStore.create({
     autoRemove:"native"
 })
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://shoppoholicc.netlify.app'); // Set your client's domain here
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.use(cors({
@@ -72,12 +77,7 @@ app.use(cors({
   credentials: true // Enable CORS credentials (cookies, authorization headers)
 
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://shoppoholicc.netlify.app'); // Set your client's domain here
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 app.use(session({
     secret: "secretkeyhaipakki",
 saveUninitialized:true,
