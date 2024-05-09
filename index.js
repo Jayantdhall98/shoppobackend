e3c7d118884b16aad8ecc980e98e6e04589f37e0
jayantdhall98 <jayantdhall98@gmail.com>
2024-05-10 01:20:48 +0530

    nodeindexxjs4rth

--- index.js
+++ index.js
@@ -72,7 +72,12 @@ app.use(cors({
   credentials: true // Enable CORS credentials (cookies, authorization headers)
 
 }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://shoppoholicc.netlify.app'); // Set your client's domain here
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(session({
     secret: "secretkeyhaipakki",
 saveUninitialized:true,
