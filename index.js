949b4df30baa18e5cd8a761bd8c31ae3222c6a71
jayantdhall98 <jayantdhall98@gmail.com>
2024-05-11 00:38:23 +0530

    nodeindexxjs4rthdddee

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-    origin: ['https://shoppoholicc.netlify.app'], // Replace with your frontend domain
+    origin: ['*'], // Replace with your frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Enable CORS credentials (cookies, authorization headers)
